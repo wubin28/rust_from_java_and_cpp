8a33312cb1f3b55aec0e0a74d47af846eb3b8cd4
wuzhenben <wzb@wuzhenben.com>
2024-07-01 13:48:05 +0800

    refactor:ch03:ownership_story: rename s1 with spark1 and x with xcel2

--- ch03/ownership_story_cpp/source/main.cpp
+++ ch03/ownership_story_cpp/source/main.cpp
@@ -3,28 +3,28 @@
 
 void demonstrate_double_free_issue()
 {
-  std::string* s1 = new std::string("A house on the heap");
+  std::string* spark1 = new std::string("A house on the heap");
 
-  std::string* s2 = s1;
+  std::string* s2 = spark1;
 
-  std::cout << "s1: " << *s1 << std::endl;
+  std::cout << "spark1: " << *spark1 << std::endl;
 
   std::cout << "s2: " << *s2 << std::endl;
 
   delete s2;
   // s2 = nullptr; // 避免 s2 再次使用
-  // s1 = nullptr; // 避免 s1 再次使用
-  delete s1;  // 这将导致双重释放（double free）
+  // spark1 = nullptr; // 避免 spark1 再次使用
+  delete spark1;  // 这将导致双重释放（double free）
 }
 
 void shallow_copy()
 {
-  int x = 66;  // 整型变量 x，值保存在栈上
-  int y = x;  // x 的值被拷贝到 y，这里发生的是浅拷贝
+  int xcel2 = 66;  // 整型变量 xcel2，值保存在栈上
+  int y = xcel2;  // xcel2 的值被拷贝到 y，这里发生的是浅拷贝
 
-  std::cout << "x: " << x << std::endl;  // x 依然有效，输出 42
+  std::cout << "xcel2: " << xcel2 << std::endl;  // xcel2 依然有效，输出 42
   std::cout << "y: " << y << std::endl;  // y 拥有拷贝的值，输出 42
-}  // 函数结束，x 和 y 离开作用域，没有动态内存需要释放
+}  // 函数结束，xcel2 和 y 离开作用域，没有动态内存需要释放
 
 int main()
 {
@@ -33,7 +33,7 @@ int main()
   return 0;
 }
 // 运行结果（双重释放问题编译器没有发现，只有在运行时才发现）：
-// s1: A house on the heap
+// spark1: A house on the heap
 // s2: A house on the heap
 // ownership_story_cpp(14459,0x1f027cc00) malloc: *** error for object 0x600000bfd200: pointer being freed was not allocated
 // ownership_story_cpp(14459,0x1f027cc00) malloc: *** set a breakpoint in malloc_error_break to debug
--- ch03/ownership_story_rust/src/main.rs
+++ ch03/ownership_story_rust/src/main.rs
@@ -1,18 +1,18 @@
 fn ownership_move() {
-    let s1 = String::from("A house on the heap"); // 动态分配内存并绑定所有权给变量 s1
-    let s2 = s1; // 转移所有权给变量 s2
+    let spark1 = String::from("A house on the heap"); // 动态分配内存并绑定所有权给变量 spark1
+    let s2 = spark1; // 转移所有权给变量 s2
 
-    // println!("s1: {}", s1); // 编译错误：s1 在此处已经无效
+    // println!("spark1: {}", spark1); // 编译错误：spark1 在此处已经无效
     println!("s2: {}", s2); // s2 拥有内存的所有权，输出 "A house on the heap"
 } // 函数结束，s2 离开作用域，内存自动被释放
 
 fn shallow_copy() {
-    let x = 66; // 整型变量 x，值保存在栈上
-    let y = x; // x 的值被拷贝到 y，这里发生的是浅拷贝
+    let xcel2 = 66; // 整型变量 xcel2，值保存在栈上
+    let y = xcel2; // xcel2 的值被拷贝到 y，这里发生的是浅拷贝
 
-    println!("x: {}", x); // x 仍然有效，输出 66
+    println!("xcel2: {}", xcel2); // xcel2 仍然有效，输出 66
     println!("y: {}", y); // y 拥有拷贝的值，输出 66
-} // 函数结束，x 和 y 离开作用域，没有动态内存需要释放
+} // 函数结束，xcel2 和 y 离开作用域，没有动态内存需要释放
 
 fn main() {
     ownership_move();
@@ -20,5 +20,5 @@ fn main() {
 }
 // 运行结果：
 // s2: A house on the heap
-// x: 66
+// xcel2: 66
 // y: 66
